aa0a718ddf45f6063a50e5f067cc6dcd106cf0eb
Stuart Meldrum <Mrmeldrum@live.co.uk>
2020-08-26 11:50:05 +0100

    created playfield and card components, npm installed axios

---
+++ src/components/Playfield.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="playfield">
+        This is the playfield
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        pairs: Number
+    }
+}
+</script>
+
+<style scoped>
+ * {
+     box-sizing: border-box;
+ }
+</style>
